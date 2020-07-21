3414c391f59428e1dbc9b7c2e09d69a0b10b12fb
agel122 <agel122@yandex.ru>
2020-07-21 12:54:01 +0300

    with print

--- hater1.py
+++ hater1.py
@@ -2,6 +2,8 @@
 print('нужно создать максимум хейта, используя минимальное количество хейтеров.\
 используем для этого жадный алгоритм.\n')
 
+import copy
+
 def inputdata(type_of_person):
     inputdata = []
     print('ввевем список %s. если %s достаточно, нажмите q\n' % (type_of_person, type_of_person))
@@ -12,6 +14,18 @@ def inputdata(type_of_person):
         else:
             inputdata.append(b)
     return inputdata
+
+who_is_hated = inputdata('хейтеримых')
+who_is_hated = set(who_is_hated)
+who_is_hated_initial = copy.copy(who_is_hated)
+
+haters = inputdata('хейтеров')
+haters = dict.fromkeys(haters, {})
+
+for name in haters.keys():
+    print('люди, которых хейтит %s' % name)
+    haters_list = inputdata('хейтеримых хейтером %s' % name)
+    haters[name] = set(haters_list)
 
 def maxcover_by_min_items(items_to_be_covered, items_to_choose):
     min_items_covers_max = set()
@@ -27,17 +41,9 @@ def maxcover_by_min_items(items_to_be_covered, items_to_choose):
         min_items_covers_max.add(best_item)
     return min_items_covers_max
 
-who_is_hated = inputdata('хейтеримых')
-who_is_hated = set(who_is_hated)
-
-haters = inputdata('хейтеров')
-haters = dict.fromkeys(haters, {})
-
-for name in haters.keys():
-    print('люди, которых хейтит %s' % name)
-    haters_list = inputdata('хейтеримых хейтером %s' % name)
-    haters[name] = set(haters_list)
-
 max_hate_by_minhaters = maxcover_by_min_items(who_is_hated, haters)
+
+print(f'вот полный список людей, которых хейтерях - {who_is_hated_initial}\n')
+print(f'вот полный список хейтеров с людьми, которых они хейтят - {haters}\n')
 print('вот минимальный список хейтеров, хейтящих максимум людей', max_hate_by_minhaters)
 
